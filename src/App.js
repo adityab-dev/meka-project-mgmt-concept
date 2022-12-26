2de9a83a1f339b9e442e219cbbf305ab947f47d8
adityab-dev <120619189+adityab-dev@users.noreply.github.com>
2022-12-26 14:23:08 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import "./App.css";
 
 import Navbar from "./Navbar/Navbar";
 import Content from "./Content/Content";
-import Temporary from "../src/Temporary";
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Content />
-      {/* <Temporary /> */}
     </div>
   );
 }
